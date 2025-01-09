253ed0cf3d19cf3112d9018a3d18c2fa041e1a4d
league <league@adminisorsimac3.attlocal.net>
2025-01-08 19:58:51 -0800

    Module 5

---
+++ src/_01_algorithms/_1_prime_or_not/primeornot.java
@@ -0,0 +1,17 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class primeornot {
+	public static void main(String[] args) {
+
+
+String number = JOptionPane.showInputDialog( "Give me a number ");
+Integer.parseInt(number);
+for(int number; number < 20; number++) {
+	if(number );
+}
+JOptionPane.showMessageDialog(null, "Your number is prime");
+JOptionPane.showMessageDialog(null, "Your number is prime");
+	}
+}
